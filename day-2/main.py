602d5fd545dda712df11142b207e96cc69326fdd
TavionK <tavion.k.britt99@gmail.com>
2022-12-02 21:15:30 -0500

    Finished part 1 of day 2

---
+++ day-2/main.py
@@ -0,0 +1,55 @@
+def rps_():
+	res = 0
+	if 'X' in x[1]:
+		if 'A' in x[0]:
+			res = 3
+		elif 'B' in x[0]:
+			res = 0
+		elif 'C' in x[0]:
+			res = 6
+	elif 'Y' in x[1]:
+		if 'A' in x[0]:
+			res = 6
+		elif 'B' in x[0]:
+			res = 3
+		elif 'C' in x[0]:
+			res = 0
+	elif 'Z' in x[1]:
+		if 'A' in x[0]:
+			res = 0
+		elif 'B' in x[0]:
+			res = 6
+		elif 'C' in x[0]:
+			res = 3
+	return(res)
+
+def auto_():
+	res = 0
+	if 'X' in x[1]:
+		res = 1
+	elif 'Y' in x[1]:
+		res = 2
+	elif 'Z' in x[1]:
+		res = 3
+	return(res)
+
+
+
+lines = []
+
+f = open("input.txt", "r")
+lines = f.readlines()
+
+score = 0
+count = 0
+
+for line in lines:
+	count += 1
+	current = 0
+	x = line.split(" ")
+	print("rps: " + str(rps_()))
+	print("auto: " + str(auto_()))
+	current = auto_() + rps_()
+	print(current)
+	score = score + current
+print(score)
