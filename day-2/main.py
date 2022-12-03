af2be165d49acb479cf48196d8b2d43f54ecce9f
TavionK <tavion.k.britt99@gmail.com>
2022-12-02 21:29:58 -0500

    finished part 2 of day 2

--- day-2/main.py
+++ day-2/main.py
@@ -33,23 +33,72 @@ def auto_():
 		res = 3
 	return(res)
 
+def outcome_():
+	res = 0
+	if 'X' in x[1]:
+		res = 0
+	elif 'Y' in x[1]:
+		res = 3
+	elif 'Z' in x[1]:
+		res = 6
+	return(res)
 
+def forced_():
+	res = 0
+	if 'X' in x[1]:
+		if 'A' in x[0]:
+			res = 3
+		elif 'B' in x[0]:
+			res = 1
+		elif 'C' in x[0]:
+			res = 2
+	elif 'Y' in x[1]:
+		if 'A' in x[0]:
+			res = 1
+		elif 'B' in x[0]:
+			res = 2
+		elif 'C' in x[0]:
+			res = 3
+	elif 'Z' in x[1]:
+		if 'A' in x[0]:
+			res = 2
+		elif 'B' in x[0]:
+			res = 3
+		elif 'C' in x[0]:
+			res = 1
+	return(res)
 
 lines = []
-
 f = open("input.txt", "r")
 lines = f.readlines()
-
 score = 0
+second = 0
 count = 0
 
 for line in lines:
 	count += 1
 	current = 0
 	x = line.split(" ")
-	print("rps: " + str(rps_()))
-	print("auto: " + str(auto_()))
 	current = auto_() + rps_()
-	print(current)
 	score = score + current
-print(score)
+
+	new = forced_() + outcome_()
+	second = second + new
+print("Answer to part one: " + str(score))
+print("Answer to part two: " + str(second))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
