95da7ac7167b9ae0cb6563a3b66edd4f8f2cbb20
TavionK <tavion.k.britt99@gmail.com>
2022-12-16 15:59:31 -0500

    completed day 6 part 2

--- day-6/main.py
+++ day-6/main.py
@@ -13,16 +13,39 @@ def getFour_():
 		tl.append(line[count+1])
 		tl.append(line[count+2])
 		tl.append(line[count+3])
-		# print(tl)
 		count += 1
 		result = checkDup_(tl)
 		if result is False:
 			print(count+3)
 			return
 
+def get14_():
+	count = 0
+	for x in line:
+		tl = []
+		dup = True
+		tl.append(line[count])
+		tl.append(line[count+1])
+		tl.append(line[count+2])
+		tl.append(line[count+3])
+		tl.append(line[count+4])
+		tl.append(line[count+5])
+		tl.append(line[count+6])
+		tl.append(line[count+7])
+		tl.append(line[count+8])
+		tl.append(line[count+9])
+		tl.append(line[count+10])
+		tl.append(line[count+11])
+		tl.append(line[count+12])
+		tl.append(line[count+13])
+		count += 1
+		result = checkDup_(tl)
+		if result is False:
+			print(count+13)
+			return
 
 f = open("input.txt", "r")
 original = f.readline()
 line = list(original)
-# print(line)
 getFour_()
+get14_()
